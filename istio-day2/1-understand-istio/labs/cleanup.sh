fec0326f63454697012bf83777072246510e2fe2
Christian Posta <christian.posta@gmail.com>
2021-02-24 06:22:03 -0700

    fixed cleanup to cleanup each lab

--- istio-day2/1-understand-istio/labs/cleanup.sh
+++ istio-day2/1-understand-istio/labs/cleanup.sh
@@ -1,3 +1,15 @@
 # delete all istio
 istioctl x uninstall -y --purge
 kubectl delete ns istio-system
+
+pushd ./01
+./cleanup.sh
+popd
+
+pushd ./02
+./cleanup.sh
+popd
+
+pushd ./03
+./cleanup.sh
+popd
