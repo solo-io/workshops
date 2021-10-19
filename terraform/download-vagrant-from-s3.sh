feb39fed2331184d11500902cf1442304169c51d
Jesus Munoz <jesus.munoz@solo.io>
2021-10-19 14:36:23 +0200

    vagrant import automated

--- terraform/download-vagrant-from-s3.sh
+++ terraform/download-vagrant-from-s3.sh
@@ -3,3 +3,4 @@ mkdir -p vagrant-workshop
 vagrant_version=$(cat vagrant-image-stable.txt)
 aws s3 cp s3://artifacts.solo.io/vagrant-images/workshop-generic-${vagrant_version}-vagrant.box vagrant-workshop/package.box
 aws s3 cp s3://artifacts.solo.io/vagrant-images/workshop-generic-${vagrant_version}-vagrant.Vagrantfile vagrant-workshop/Vagrantfile
+vagrant box add --force packer_solo_image vagrant-workshop/package.box
