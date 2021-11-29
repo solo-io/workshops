293a437443d5c73401485cb5e51d6f4d748ff526
Jesus Munoz <jesus.munoz@solo.io>
2021-11-29 19:01:48 +0100

    kind version bump

---
+++ terraform/build-instruqt-gloo-edge-image.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd packer/instruqt-gloo-edge
+packer build -force -color=true -timestamp-ui .
---
+++ terraform/build-instruqt-gloo-mesh-image.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd packer/instruqt-gloo-mesh
+packer build -force -color=true -timestamp-ui .
