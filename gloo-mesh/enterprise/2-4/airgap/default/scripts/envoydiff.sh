ff51564fab32e46953ab891fd3c4cd6c2f9f0850
Jesus Munoz <jesus.munoz@solo.io>
2024-03-26 16:05:11 +0100

    2-6 cleanup

--- gloo-mesh/enterprise/2-4/airgap/default/scripts/envoydiff.sh
+++
@@ -1,3 +0,0 @@
-mv /tmp/current-output /tmp/previous-output 2>/dev/null
-./istio-*/bin/istioctl --context cluster1 pc all -n istio-gateways deploy/istio-ingressgateway -o json > /tmp/current-output
-json-diff /tmp/previous-output /tmp/current-output
